3f5341ee16ed42fb7ef52193722ec7097ea30c9e
sachinm007 <bsachinmiryala@gmail.com>
2025-11-08 18:34:25 -0700

    updated project structure and requirements

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="nasa anomaly detection",
+    version="0.01",
+    author="Sachin Miryala",
+    email="bsachinmiryala@gmail.com",
+    packages=find_packages()
+)
